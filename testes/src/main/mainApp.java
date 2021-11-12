27fde3ccae1c70577936544af259691236710126
Gabriel Oliveira <77310038+GS-Moonlyn@users.noreply.github.com>
2021-11-12 17:57:16 -0300

    Revert "Commit Update"
    This reverts commit ab6edad2aa9f4a926ae8b6f90fd7a473e7641f0e.

--- testes/src/main/mainApp.java
+++ testes/src/main/mainApp.java
@@ -2,13 +2,7 @@ package main;
 
 public class mainApp {
 	
-<<<<<<< Updated upstream
 	private String nome;
-=======
-	private int data;
-	private string nome;
-	private int idade;
->>>>>>> Stashed changes
 	
 	public static void main(String[] args) {
 		System.out.println("Teste");
