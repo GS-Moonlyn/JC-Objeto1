ab6edad2aa9f4a926ae8b6f90fd7a473e7641f0e
DanielCabralBarbosa <77117506+DanielCabralBarbosa@users.noreply.github.com>
2021-11-12 17:49:49 -0300

    Commit Update
    Tentativa de merge com conflito.

--- testes/src/main/mainApp.java
+++ testes/src/main/mainApp.java
@@ -2,7 +2,13 @@ package main;
 
 public class mainApp {
 	
+<<<<<<< Updated upstream
 	private String nome;
+=======
+	private int data;
+	private string nome;
+	private int idade;
+>>>>>>> Stashed changes
 	
 	public static void main(String[] args) {
 		System.out.println("Teste");
